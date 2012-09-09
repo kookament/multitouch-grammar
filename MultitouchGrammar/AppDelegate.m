ee55dd41d2d6bbe84ce8c11abfb19041e2d63b45
kookament <@>
2012-09-09 15:58:34 -0700

    Initial Commit

---
+++ MultitouchGrammar/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  MultitouchGrammar
+//
+//  Created by Sean Kelley on 9/9/12.
+//  Copyright (c) 2012 Sean Kelley. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ MultitouchGrammar/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MultitouchGrammar
+//
+//  Created by Sean Kelley on 9/9/12.
+//  Copyright (c) 2012 Sean Kelley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
