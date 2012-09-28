f0d4eeb67db7056fcdff03e9e3328039a73c302e
kookament <@>
2012-09-28 00:59:16 -0400

    Added some configuration parameters to movement detection.

--- MultitouchGrammar/Touch.m
+++ MultitouchGrammar/Touch.m
@@ -1,9 +1,23 @@
 #import "Touch.h"
 
-// How far a finger must move (in normalized space) for directionFrom:withBias: to
-// consider the distance nonaccidental/twitchy. If the distance is less than this
-// threshold, directionFrom:withBias: returns Direction.NONE.
+// How far a finger must move (in normalized space, after X_BIAS application) for
+// directionFrom:withBias: to consider the distance nonaccidental/twitchy. If the
+// distance is less than this threshold, directionFrom:withBias: returns
+// Direction.NONE.
+// Determined experimentally.
 const float MIN_DISTANCE = 0.05;
+// When a bias direction is given to directionFrom:withBias:, how strongly it
+// should affect the outcome. 1 has no effect, >1 is more bias, and <1 is reverse
+// bias (and should not be used -- though this is not enforced).
+// Determined experimentally.
+const float BIAS_FACTOR = 2;
+// A compensation factor for movement in the x direction to undo normalization:
+// fingers may move more in the x than y direction, but because the rectangular
+// trackpad is normalized, this may confusingly register as more movement in the
+// y direction.
+// Determined by measuring the trackpad approximately. May not be appropriate
+// for all trackpad sizes.
+const float X_BIAS = 1.5;
 
 @interface Touch () {
     mtTouch *sourceTouch;
@@ -35,13 +49,13 @@ const float MIN_DISTANCE = 0.05;
     if (origin == nil)
         return Direction.NONE;
     
-    float xdiff = self.x - origin.x;
+    float xdiff = (self.x - origin.x) * X_BIAS;
     float ydiff = self.y - origin.y;
     
     if (bias == Direction.LEFT || bias == Direction.RIGHT)
-        xdiff *= 2;
+        xdiff *= BIAS_FACTOR;
     else if (bias == Direction.UP || bias == Direction.DOWN)
-        ydiff *= 2;
+        ydiff *= BIAS_FACTOR;
     
     if (fabs(xdiff) > fabs(ydiff) && fabs(xdiff) > MIN_DISTANCE)
         return xdiff < 0 ? Direction.LEFT : Direction.RIGHT;
